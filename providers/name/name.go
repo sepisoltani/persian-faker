6f6a6c3a9bd50fd53b3eb13a346409634cf96d79
Sepehr <sepehr.develop@gmail.com>
2024-04-27 02:37:15 +0200

    Add dependency injection for the data loader and the random number generator

--- providers/name/name.go
+++ providers/name/name.go
@@ -7,41 +7,59 @@ import (
 	"path/filepath"
 )
 
+// NameData represents the structure for names data.
 type NameData struct {
 	FirstNames []string `json:"firstNames"`
 	LastNames  []string `json:"lastNames"`
 }
 
-var (
-	nameData  NameData
-	localRand *rand.Rand
-)
-
-func init() {
-	localRand = rand.New(rand.NewSource(int64(os.Getpid())))
-	loadData()
+// DataLoader is the interface for loading name data.
+type DataLoader interface {
+	LoadData(path string) (NameData, error)
 }
 
-func loadData() {
+// FileDataLoader loads name data from a file.
+type FileDataLoader struct{}
+
+func (fdl FileDataLoader) LoadData(path string) (NameData, error) {
+	var data NameData
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return data, err
+	}
+	err = json.Unmarshal(file, &data)
+	return data, err
+}
+
+// NameGenerator holds the loaded name data and the random generator.
+type NameGenerator struct {
+	Data NameData
+	Rand *rand.Rand
+}
+
+// NewNameGenerator creates a new NameGenerator with data loaded using the given DataLoader and random source.
+func NewNameGenerator(loader DataLoader, seed int64) (*NameGenerator, error) {
 	dataPath := filepath.Join("data", "names.json")
-	file, err := os.ReadFile(dataPath)
+	data, err := loader.LoadData(dataPath)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	err = json.Unmarshal(file, &nameData)
-	if err != nil {
-		panic(err)
-	}
+	src := rand.NewSource(seed)
+	rng := rand.New(src)
+	return &NameGenerator{Data: data, Rand: rng}, nil
 }
 
-func RandomFirstName() string {
-	return nameData.FirstNames[localRand.Intn(len(nameData.FirstNames))]
+// RandomFirstName returns a random Persian first name.
+func (ng *NameGenerator) RandomFirstName() string {
+	return ng.Data.FirstNames[ng.Rand.Intn(len(ng.Data.FirstNames))]
 }
 
-func RandomLastName() string {
-	return nameData.LastNames[localRand.Intn(len(nameData.LastNames))]
+// RandomLastName returns a random Persian last name.
+func (ng *NameGenerator) RandomLastName() string {
+	return ng.Data.LastNames[ng.Rand.Intn(len(ng.Data.LastNames))]
 }
 
-func RandomFullName() string {
-	return RandomFirstName() + " " + RandomLastName()
+// RandomFullName returns a random Persian full name.
+func (ng *NameGenerator) RandomFullName() string {
+	return ng.RandomFirstName() + " " + ng.RandomLastName()
 }
